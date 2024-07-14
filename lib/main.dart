0c213b433a182b8365bf825b2d747b3d462cb2c5
Matt <matt.garvin.416@gmail.com>
2024-07-14 00:07:51 -0400

    added keyboard input

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-//  This code is literally from the "flutter create" boilerplate,
-//  but I removed the counter app (and kept the underlying structure),
-//  and some unecessary comments and variables...
+//  See:
+//  https://api.flutter.dev/flutter/widgets/CallbackShortcuts-class.html
 
 void main() {
   runApp(const MyApp());
@@ -33,28 +33,78 @@ class _MyHomePageState extends State<MyHomePage> {
 
   //  page vars
   final String filename = 'main.dart';
+  String key = '';
 
   @override
   void initState() {
-    //  anything?
+    //HardwareKeyboard.instance.addHandler(_handleKey);
     super.initState();
   }
 
   @override
+  void dispose() {
+    //HardwareKeyboard.instance.removeHandler(_handleKey)
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        appBar: AppBar(
-          title: Text( filename ),
-          centerTitle: true,
-        ),
-        body: Container(
-          color: Colors.transparent,
-          child: Center(
-            child: Text( '$filename is ready!'),
+    return CallbackShortcuts(
+      bindings: <ShortcutActivator, VoidCallback> {
+         const SingleActivator(LogicalKeyboardKey.digit0): () {
+          setState(() { key = '0'; });
+        },       
+        const SingleActivator(LogicalKeyboardKey.digit1): () {
+          setState(() { key = '1'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.digit2): () {
+          setState(() { key = '2'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.digit3): () {
+          setState(() { key = '3'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.digit4): () {
+          setState(() { key = '4'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.digit5): () {
+          setState(() { key = '5'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.digit6): () {
+          setState(() { key = '6'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.digit7): () {
+          setState(() { key = '7'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.digit8): () {
+          setState(() { key = '8'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.digit9): () {
+          setState(() { key = '9'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.enter): () {
+          setState(() { key = 'ENTER'; });
+        },
+        const SingleActivator(LogicalKeyboardKey.backspace): () {
+          setState(() { key = 'BACKSPACE'; });
+        },
+      },
+      child: Focus(
+        autofocus: true,
+        child: Material(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('View NUMERIC keypresses in'),
+                const Text('the console (plus BACKSPACE and ENTER)'),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0,20,0,0),
+                  child: Text('"$key"'),
+                ),
+              ],
+            ),
           ),
-        ),  
+        ),
       ),
     );
   }
